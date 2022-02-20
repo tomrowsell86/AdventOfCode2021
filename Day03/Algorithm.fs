9a8cba6b96c825782dd237915aec277798acfebd
Tom Rowsell <rowselltom@gmail.com>
2022-02-20 17:43:09 +0000

    Finished part a

--- Day03/Algorithm.fs
+++ Day03/Algorithm.fs
@@ -2,17 +2,31 @@
 namespace Aoc.Day03
 open System
 module Algorithm =
-    let parseBits (s:string) = Array.map (fun v -> int (Char.GetNumericValue(v))) (s.ToCharArray()) 
-    let countOne (x:int*int) = if snd x = 1 then fst x + 1 else fst x    
-    let folder (value:string) (state:int * int[]) = 
-        let splitBits = parseBits value
-        let fn innerState =  ((fst innerState) + 1 ,Array.zip (snd innerState) splitBits |> Array.map countOne) 
-        if Array.isEmpty (snd state) 
-        then
-            fn (0,Array.replicate (splitBits.Length) 0) 
-        else 
-            fn state
-    let getColumnCounts (codes:seq<string>) = Seq.foldBack folder codes (0,Array.empty)
+    let getColumnCounts (codes:seq<string>) = 
+        let folder (value:string) (state:int * int[]) = 
+            let parseBits (s:string) = Array.map (fun v -> int (Char.GetNumericValue(v))) (s.ToCharArray()) 
+            let countOne (x:int*int) = if snd x = 1 then fst x + 1 else fst x    
+            let splitBits = parseBits value
+            let fn innerState =  ((fst innerState) + 1 ,Array.zip (snd innerState) splitBits |> Array.map countOne) 
+            if Array.isEmpty (snd state) 
+            then
+                fn (0,Array.replicate (splitBits.Length) 0) 
+            else 
+                fn state
+        Seq.foldBack folder codes (0,Array.empty)
+    let msbs count bits =
+        let mapper total b = if total - b < total/2 then 1 else 0 
+        Array.map (mapper count) bits
+
+    let binToDec (bits:uint[]) = 
+        Seq.zip (Seq.rev(seq{0..bits.Length-1})) bits 
+        |> Seq.where (fun (_,b) -> b <> 0u)
+        |> Seq.map fst
+        |> Seq.map (fun a-> Math.Pow(2,a)) 
+        |> Seq.sum
+
+     
+
 
 module Data =
     let TextLines = System.IO.File.ReadLines("input.txt");
--- Day03/Program.fs
+++ Day03/Program.fs
@@ -2,6 +2,11 @@ open Aoc.Day03
 
 [<EntryPoint>]
 let main argv =
-    let result = Algorithm.getColumnCounts Data.TextLines
-    Array.iter (fun x -> printfn "%d" x ) (snd result)
+    let (total,counts) = Algorithm.getColumnCounts Data.TextLines
+    let msbs = (Algorithm.msbs total counts) |> Array.map uint
+    let lsbs = Array.map (fun (x:uint) -> if x = 0u then 1u else 0u) msbs 
+    let gammaRate = uint (Algorithm.binToDec msbs)
+    let epsilonRate = uint (Algorithm.binToDec lsbs)
+    printfn "Result is : %d " (gammaRate * epsilonRate) 
+    
     0 
